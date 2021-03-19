c22d0374069a44b325f976d6e032e103ab9eb3ec
Yhamagee <temitopeojo0@gmail.com>
2021-03-20 00:33:12 +0100

    fixed issue

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Link from "next/link"
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa"
+import Image from "next/image"
+
 
 const Footer = () => {
 
@@ -8,8 +10,9 @@ const Footer = () => {
         <div className="text-white py-5 px-3 flex justify-center bg-gradient-to-br from-red-400 via-pink-500 to-red-500">
             <div className="my-5 mx-3 flex flex-col max-w-7xl justify-between md:flex-row border-t-2 border-white">
                 <div className="m-2 max-w-md">
-                    <div className="flex flex-col md:flex-row items-center">
-                        <img className="w-10 h-10 object-cover m-2" src="/quoteImage.jpg" />
+                    <div className="flex flex-col md:flex-row items-center space-x-3">
+                        <Image height="150" width="150" layout="intrinsic"
+                         className="w-10 h-10 object-cover" src="/quoteImage.jpg" />
                         <p className="my-2 text-center md:text-left">
                             It is a long established fact that a reader will be distracted by the readable content
                             of a page when looking at its layout. The point of using Lorem Ipsum is that it.
